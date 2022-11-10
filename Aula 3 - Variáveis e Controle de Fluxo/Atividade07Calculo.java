de8220f99da7e1dc148cdf9d2bd31346ccf96f25
Julia Leão <104568516+juliaaliceleao@users.noreply.github.com>
2022-11-10 10:28:26 -0300

    Create Atividade07Calculo.java

---
+++ Aula 3 - Variáveis e Controle de Fluxo/Atividade07Calculo.java
@@ -0,0 +1,19 @@
+public class Atividade07Calculo
+{
+    public static void main(String args[])
+    {
+        //Variables Statements
+        int x = 3;
+
+        //if Conditional
+        if(x % 2 == 0)
+        {x = x/2; }
+
+        
+        else
+
+        { x = (3 * x) + 1; }
+
+        System.out.print(x + "\n");
+    }
+}
